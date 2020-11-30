5c5c0867991316ca06c5b2466c9fdb50d3dd9765
Justin Goodchild <jeg6300@gmail.com>
2020-11-29 23:51:40 -0500

    javascript now properly checks to ensure the given input contains texts before adding to the div

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($){
     $( '.converter' ).click(function( event ) {
         var input = document.getElementById('inputText');
-        $('.tasks').prepend('<p>' + input.value + '<p>');
+        var textInput = input.value;
+        if(textInput && textInput.trim()){
+            $('.tasks').prepend('<p>' + input.value + '</p>');
+        }
     });
 });
